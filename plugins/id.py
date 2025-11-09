b3c8dc58a0c5fd1253e90d2e64f04ea3fca5838d
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 01:21:23 +0700

    Add command to get user or chat ID

---
+++ plugins/id.py
@@ -0,0 +1,10 @@
+from telethon import events
+from main import client
+
+@client.on(events.NewMessage(outgoing=True, pattern=r"^\.id$"))
+async def get_id(event):
+    if event.reply_to_msg_id:
+        reply = await event.get_reply_message()
+        await event.respond(f"👤 ID Pengguna: `{reply.sender_id}`")
+    else:
+        await event.respond(f"💬 ID Chat: `{event.chat_id}`")
