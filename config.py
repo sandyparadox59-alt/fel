1ff46e0efbbb4c271547e9692715cb4d1571848a
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 01:19:16 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# === KONFIGURASI USERBOT ===
+api_id = 33931529     # isi API ID dari https://my.telegram.org
+api_hash = "7bdc34f542e3a6eff7558646271e59ec"  # isi API HASH
+session_name = "userbot"  # nama file session yang tersimpan otomatis
