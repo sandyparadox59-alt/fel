a17953d3d441fa8bf1bd7dc279ab83af99608aff
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 12:49:30 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,9 +1,6 @@
 from telethon import events
-import time
 
-@events.register(events.NewMessage(outgoing=True, pattern=r"\.ping"))
-async def ping_cmd(event):
-    start = time.time()
-    msg = await event.respond("🏓 Pong!")
-    end = time.time() - start
-    await msg.edit(f"🏓 Pong! `{round(end * 1000)}ms`")
+def setup(client):
+    @client.on(events.NewMessage(outgoing=True, pattern=r"\.owner"))
+    async def owner(event):
+        await event.respond("👑 Owner: @GlobalBotzXD")
