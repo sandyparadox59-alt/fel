ae345a057cb23e15ed87d2b76201a66b759dd7b4
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 13:40:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID = 33931529  # ambil dari https://my.telegram.org
-API_HASH = "7bdc34f542e3a6eff7558646271e59ec"
+API_ID = 123  # ambil dari https://my.telegram.org
+API_HASH = "xxxx"
 SESSION = "userbot"  # nama file session (userbot.session)
-OWNER_ID = 8052207089  # ID Telegram owner
+OWNER_ID = 123  # ID Telegram owner
