fa4d5b8d55f87fedbfdf169154eb1cb7e4771459
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 13:42:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ API_ID = 123  # ambil dari https://my.telegram.org
 API_HASH = "xxxx"
 SESSION = "userbot"  # nama file session (userbot.session)
 OWNER_ID = 123  # ID Telegram owner
+RESELLERS = [1234567890, 987654321]  # ID Telegram reseller
