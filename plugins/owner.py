943978eb09b538cca50c4e152953bcaeea4eb8f0
sandyparadox59-alt <sandyparadox59@gmail.com>
2025-11-10 12:48:18 +0700

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -1,6 +1,10 @@
 from telethon import events
-from config import OWNER_ID
 
-@events.register(events.NewMessage(outgoing=True, pattern=r"\.owner"))
-async def owner_cmd(event):
-    await event.respond(f"👑 **Owner ID:** `{OWNER_ID}`")
+def setup(client):
+    @client.on(events.NewMessage(outgoing=True, pattern=r"\.id"))
+    async def id(event):
+        reply = await event.get_reply_message()
+        if reply:
+            await event.respond(f"🆔 ID Reply: `{reply.sender_id}`")
+        else:
+            await event.respond(f"🆔 ID Kamu: `{event.sender_id}`\n💬 Chat ID: `{event.chat_id}`")
